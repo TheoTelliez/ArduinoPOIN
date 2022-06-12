291db3d558374dd06d0cfa71fa574c08a804b0e0
TheoTELLIEZ <theo.telliez@epsi.fr>
2022-06-12 16:22:20 +0200

    Add Air Quality Sensor

--- POIN.ino
+++ POIN.ino
@@ -9,9 +9,23 @@ String resultMoistureSensor = String();
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
 #define DHTPIN 2     // what pin we're connected to（DHT10 and DHT20 don't need define it）
 DHT dht(DHTPIN, DHTTYPE);
+
+// AIR QUALITY SENSOR
+#include "Air_Quality_Sensor.h"
+AirQualitySensor airsensor(A0);
  
 void setup() {
     Serial.begin(9600);
+
+    // AIR QUALITY SENSOR
+    Serial.println("Waiting sensor to init...");
+    //delay(20000);
+
+    if (airsensor.init()) {
+        Serial.println("Sensor ready.");
+    } else {
+        Serial.println("Sensor ERROR!");
+    }
 }
 void loop() {
     // MOISTURE SENSOR
@@ -39,5 +53,21 @@ void loop() {
       Serial.println(" *C\n");
     } else {
         Serial.println("Failed to get temprature and humidity value.");
+    }
+
+    // AIR QUALITY SENSOR
+    int quality = airsensor.slope();
+
+    Serial.print("Sensor value: ");
+    Serial.println(airsensor.getValue());
+
+    if (quality == AirQualitySensor::FORCE_SIGNAL) {
+        Serial.println("High pollution! Force signal active.\n");
+    } else if (quality == AirQualitySensor::HIGH_POLLUTION) {
+        Serial.println("High pollution!\n");
+    } else if (quality == AirQualitySensor::LOW_POLLUTION) {
+        Serial.println("Low pollution!\n");
+    } else if (quality == AirQualitySensor::FRESH_AIR) {
+        Serial.println("Fresh air.\n");
     }
 }
