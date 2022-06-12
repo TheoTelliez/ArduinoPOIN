1d9d882f298393a604961292e5c1689b2f7cc3b6
TheoTELLIEZ <theo.telliez@epsi.fr>
2022-06-12 15:21:25 +0200

    Moisture sensor implementation

--- POIN.ino
+++ POIN.ino
@@ -1,9 +1,21 @@
+int sensorPin = A0;
+int sensorMoistureValue = 0;
+String resultMoistureSensor = String();
+ 
 void setup() {
-  // put your setup code here, to run once:
-
+    Serial.begin(9600);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    // read the value from the sensor:
+    sensorMoistureValue = analogRead(sensorPin);
+    delay(1000);
+    if(0 <= sensorMoistureValue and sensorMoistureValue < 300){
+      resultMoistureSensor = "Dry";
+    } else if (300 <= sensorMoistureValue and sensorMoistureValue < 600){
+      resultMoistureSensor = "Moist";
+    } else{
+      resultMoistureSensor = "Wet";
+    };
+    Serial.print("Moisture = " + resultMoistureSensor + " : " + sensorMoistureValue + "\n");
+            
 }
